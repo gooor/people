a9159a0b761ed16c8991b3ac4f81fba20841035b
mxaly <rafal.maly@gmail.com>
2016-12-05 09:28:14 +0100

    set defaults

--- db/migrate/20161204134203_create_skills.rb
+++ db/migrate/20161204134203_create_skills.rb
@@ -3,7 +3,7 @@ class CreateSkills < ActiveRecord::Migration
     create_table :skills do |t|
       t.string :name
       t.string :description
-      t.integer :rate_type
+      t.integer :rate_type, default: 0
       t.references :skill_category, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/migrate/20161204191635_create_user_skill_rates.rb
+++ db/migrate/20161204191635_create_user_skill_rates.rb
@@ -3,7 +3,7 @@ class CreateUserSkillRates < ActiveRecord::Migration
     create_table :user_skill_rates do |t|
       t.references :user, index: true, foreign_key: true
       t.references :skill, index: true, foreign_key: true
-      t.integer :rate
+      t.integer :rate, default: 0
 
       t.timestamps null: false
     end
