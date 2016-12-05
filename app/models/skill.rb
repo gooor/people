efd6a8642cf2edaa41183fb6a415c1e2af4e512e
mxaly <rafal.maly@gmail.com>
2016-12-05 09:28:54 +0100

    add validations & enums to skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,17 @@
 class Skill < ActiveRecord::Base
-  belongs_to :skill_category
+  enum rate_type: %i(range boolean)
 
+  belongs_to :skill_category
   has_many :user_skill_rates
   has_many :users, through: :user_skill_rates
+
+  validates :name, :skill_category, :rate_type, presence: true
+  validate :uniques
+
+  private
+
+  def uniques
+    return unless self.class.where(name: name, skill_category_id: skill_category_id).exists?
+    errors.add('name & skill_category', 'must be uniq')
+  end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,4 +3,20 @@ require 'spec_helper'
 describe Skill do
   it { is_expected.to belong_to :skill_category }
   it { is_expected.to have_many :users }
+
+  it { is_expected.to  validate_presence_of :name }
+  it { is_expected.to  validate_presence_of :skill_category }
+  it { is_expected.to  validate_presence_of :rate_type }
+
+  describe 'uniques validation' do
+    let(:category) { create(:skill_category) }
+
+    before { create(:skill, name: 'foo', skill_category: category) }
+
+    it 'ensurs uniques by name & category' do
+      skill = build(:skill, name: 'foo', skill_category: category)
+      expect(skill.valid?).to be false
+      expect(skill.errors.messages).to eq :'name & skill_category' => ['must be uniq']
+    end
+  end
 end
